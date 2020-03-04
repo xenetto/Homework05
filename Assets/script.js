c3bc75b0ddb77942c7fe2322f1e61c30bf0af6d4
Sahar <sahar.sagharichi@gmail.com>
2020-03-04 09:14:05 +0330

    some changes to improve the code and fixing a bug related to 12PM work list box

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,13 @@
-document.getElementById("date").innerHTML=  `${moment().format("dddd, MMMM Do ")}`;   //insert date at the header of the page
+document.getElementById("date").innerHTML=  `${moment().format("dddd, MMMM Do ")}`;   //insert current date at the header of the page
 
 pageLoad();       //start point
 
 function pageLoad(){
-    setColor();
-    var workListArr = fetchFromLocalStorage(moment().format("YYYY-MM-DD"));
+    setColor();   //make current hour red, last hours light gray, next hours green
 
+    var workListArr = fetchFromLocalStorage(moment().format("YYYY-MM-DD"));  //fetched array from local storage
+
+//showing array info(saved work list) in related text boxes
     document.getElementById('txt_9').value = workListArr[6].split("|")[1]; 
     document.getElementById('txt_10').value = workListArr[7].split("|")[1];
     document.getElementById('txt_11').value = workListArr[8].split("|")[1];
@@ -16,22 +18,24 @@ function pageLoad(){
     document.getElementById('txt_4').value = workListArr[4].split("|")[1];
     document.getElementById('txt_5').value = workListArr[5].split("|")[1];
 
-    document.getElementById('btn_9').addEventListener('click', saveBtn);
-    document.getElementById('btn_10').addEventListener('click', saveBtn);
-    document.getElementById('btn_11').addEventListener('click', saveBtn);
-    document.getElementById('btn_0').addEventListener('click', saveBtn);
-    document.getElementById('btn_1').addEventListener('click', saveBtn);
-    document.getElementById('btn_2').addEventListener('click', saveBtn);
-    document.getElementById('btn_3').addEventListener('click', saveBtn);
-    document.getElementById('btn_4').addEventListener('click', saveBtn);
-    document.getElementById('btn_5').addEventListener('click', saveBtn);
+//defining click event of save buttons
+    document.getElementById('btn_9').addEventListener('click',saveBtn);
+    document.getElementById('btn_10').addEventListener('click',saveBtn);
+    document.getElementById('btn_11').addEventListener('click',saveBtn);
+    document.getElementById('btn_0').addEventListener('click',saveBtn);
+    document.getElementById('btn_1').addEventListener('click',saveBtn);
+    document.getElementById('btn_2').addEventListener('click',saveBtn);
+    document.getElementById('btn_3').addEventListener('click',saveBtn);
+    document.getElementById('btn_4').addEventListener('click',saveBtn);
+    document.getElementById('btn_5').addEventListener('click',saveBtn);
 }
 
+//saving entered work list
 function saveBtn(event){
     addToLocalStorage( moment().format("YYYY-MM-DD"),  (event.currentTarget.id).substring(4), document.getElementById(`txt_${(event.currentTarget.id).substring(4)}`).value );
-    workListArr = fetchFromLocalStorage(moment().format("YYYY-MM-DD"), (event.currentTarget.id).split("_")[1]);
 }
 
+//adding entered work list to local storage
 function addToLocalStorage(name, key, value) {
 
     var existing = localStorage.getItem(name);   // Get the existing data
@@ -45,6 +49,7 @@ function addToLocalStorage(name, key, value) {
     localStorage.setItem(name, JSON.stringify(existing));    // Save back to localStorage
 }
 
+//fetching entered work list from local storage
 function fetchFromLocalStorage(name, key = "") {
     
     var existing = localStorage.getItem(name);   // Get the existing data
@@ -62,12 +67,14 @@ function fetchFromLocalStorage(name, key = "") {
     return myArrList;
 }
 
+//setting each row color according to current hour
 function setColor(){
     var hour = moment().format("h");   
-    var AMPM = moment().format("A");           
+    var AMPM = moment().format("A");
 
-    if((hour>=9 && hour<=12 && AMPM=="AM") || (hour>=1 && hour<=5 && AMPM=="PM")){
-        document.getElementById(`txt_${moment().format("h")}`).style.backgroundColor="red";
+//between 9AM to 5PM
+    if((hour>=9 && hour<12 && AMPM=="AM") || ((hour ==12 || hour>=1 && hour<=5) && AMPM=="PM")){
+        document.getElementById(`txt_${moment().format("h")%12}`).style.backgroundColor="red";
         for(i=9; i<moment().format("H");i++){
             document.getElementById(`txt_${i%12}`).style.backgroundColor="lightgray";
             document.getElementById(`txt_${i%12}`).readOnly=true;
@@ -79,6 +86,7 @@ function setColor(){
         }
 
     }
+//before 9AM and after 5PM    
     else{
         var x;
         x = document.querySelectorAll(".textBx");
